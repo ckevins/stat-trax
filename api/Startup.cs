2c229d5fbd80af157acb546e5117c2672bbced40
Cody Evins <cody.evins@gmail.com>
2021-10-02 17:33:56 -0400

    API with working routes

--- api/Startup.cs
+++ api/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +18,9 @@ namespace api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            var currentAssembly = Assembly.GetExecutingAssembly();
+            services.AddMediatR(currentAssembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +30,10 @@ namespace api
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            app.UseEndpoints((endpoints) => 
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
