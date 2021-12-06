6c72b76c02d6e366558c3f3af88a9e812d48aa94
Cody Evins <cody.evins@gmail.com>
2021-12-05 19:33:22 -0500

    Apollo and json-graphql-server working on league home page

--- stat-trax-vue2/src/main.js
+++ stat-trax-vue2/src/main.js
@@ -1,17 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-// import ApolloClient from 'apollo-boost'
+import ApolloClient from 'apollo-boost'
+import VueApollo from 'vue-apollo'
 
-// const apolloClient = new ApolloClient({
-//   // You should use an absolute URL here
-//   uri: 'https://api.graphcms.com/simple/v1/awesomeTalksClone'
-// })
+const apolloClient = new ApolloClient({
+  // You should use an absolute URL here
+  uri: 'http://localhost:3000'
+})
 
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueApollo);
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
 
 new Vue({
   router,
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app')
--- stat-trax-vue2/src/views/MyLeague/LeagueHome.vue
+++ stat-trax-vue2/src/views/MyLeague/LeagueHome.vue
@@ -1,39 +1,57 @@
 <template>
-    <div class='league-home'>
-        <h2>{{ dummyData.leagueName }}</h2>
-        <p>This page will display highlights of the standings and statistics pages in a more condensed format. The implication of that is that this all of our data for those pages will need to be lifted to MyLeague and passed down as props.</p>
-    </div>
+  <div class="league-home">
+    <h2>{{ League.name }}</h2>
+    <h3 v-for="(division, index) in League.Divisions" :key=index>{{ division.name }}</h3>
+    <p>
+      This page will display highlights of the standings and statistics pages in
+      a more condensed format. The implication of that is that this all of our
+      data for those pages will need to be lifted to MyLeague and passed down as
+      props.
+    </p>
+  </div>
 </template>
 
 <script>
-export default {
-    data: function() {
-        return {
-            dummyData: {
-                leagueName: "Atlanta Area Senior Men's Baseball League"
-            }
+import gql from "graphql-tag";
+
+const leagueQuery = gql`
+  query {
+    League(id: 1) {
+      name
+      Divisions {
+        name
+        Teams {
+            name
         }
+      }
     }
-}
+  }
+`;
+
+export default {
+  apollo: {
+    League: leagueQuery,
+  },
+};
 </script>
 
 
 <style scoped>
 h2 {
-    text-align: center;
+  text-align: center;
 }
 .league-home {
-    width: 98%;
-    min-height: 55vh;
-    margin: -5px auto 20px auto;
-    border-radius: 10px;
-    background-color: #4d926d;
-    padding: 10px 0 30px 0;
-    border: 5px solid #1E392A;
+  width: 98%;
+  min-height: 55vh;
+  margin: -5px auto 20px auto;
+  border-radius: 10px;
+  background-color: #4d926d;
+  padding: 10px 10px 30px ;
+  border: 5px solid #1e392a;
 }
 
 p {
-    width: 60%;
-    margin: auto;
+  width: 60%;
+  margin: auto;
 }
 </style>
